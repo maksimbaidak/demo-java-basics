1dc551d5e15ae9830c84cf266e6b9392a380e932
baidakm <maksimbaidak@gmail.com>
2024-10-21 20:17:01 +0300

    add method open

--- src/main/java/org/itacademy/baidakm/pages/BasePage.java
+++ src/main/java/org/itacademy/baidakm/pages/BasePage.java
@@ -3,4 +3,8 @@ package org.itacademy.baidakm.pages;
 public class BasePage {
 
     protected String BASE_URL = "BASE URL";
+
+    public void open(){
+        System.out.println("Пользователь открыл страницу " + BASE_URL);
+    }
 }
